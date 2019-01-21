0aa45841095b0440f30f6e58c5aac8a3e844057b
hizumisen <nagakami@gmail.com>
2019-01-21 22:49:04 +0100

    Fix env variable

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  baseUrl: process.env.NODE_ENV === 'production' ? '/spark-submit/' : '/'
+  baseUrl: process.env.NODE_ENV === 'production' ? '/spark-submit/' : '/',
   domain: process.env.NODE_ENV === 'production' ? 'hizumisen.gitlab.io' : 'localhost:8080'
 }
