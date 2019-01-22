3fe88842b8091699d4ef274a994aa9980427d71a
hizumisen <nagakami@gmail.com>
2019-01-22 22:10:03 +0100

    Add domain

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
+var webpack = require('webpack');
+
 module.exports = {
-  baseUrl: process.env.NODE_ENV === 'production' ? '/spark-submit/' : '/',
-  domain: process.env.NODE_ENV === 'production' ? 'hizumisen.gitlab.io' : 'localhost:8080'
+  baseUrl: process.env.NODE_ENV === 'production' ? '/spark-submit/' : '/'
 }
